ac38eb8a8b87a43fc4adc3e457153ba0f8a6f2f5
Nemadur <jan.piekos@gmail.com>
2019-10-16 17:10:40 +0200

    structure

---
+++ app/components/fight/fight.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class FightComponent {
+  title = 'AngularRPG';
+
+  boo: boolean = true;
+
+  arrayOfThings: string[] = [
+    'string 1',
+    'string 2',
+    'string 3'
+  ];
+
+  onClick(sentence: string){
+    console.log(`you clicked this! ${sentence}`);
+  }
+}
---
+++ app/components/start/start.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class StartComponent {
+  title = 'AngularRPG';
+
+  boo: boolean = true;
+
+  arrayOfThings: string[] = [
+    'string 1',
+    'string 2',
+    'string 3'
+  ];
+
+  onClick(sentence: string){
+    console.log(`you clicked this! ${sentence}`);
+  }
+}
